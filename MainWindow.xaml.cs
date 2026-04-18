ea1474efc34654c5b371baa3444aa150fb9e32e6
Полина Смирнова <pnsmirno@gmail.com>
2026-04-18 10:15:57 +0300

    Логика

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace МоеРадио
+{
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer player = new MediaPlayer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Кнопка СТАРТ
+        private void Старт_Click(object sender, RoutedEventArgs e)
+        {
+            // Берём ссылку из текстового поля
+            string radioUrl = UrlTextBox.Text;
+
+            // Проверяем, что ссылка не пустая
+            if (!string.IsNullOrEmpty(radioUrl))
+            {
+                try
+                {
+                    player.Open(new Uri(radioUrl));
+                    player.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите ссылку на радиостанцию!");
+            }
+        }
+
+        // Кнопка СТОП
+        private void Стоп_Click(object sender, RoutedEventArgs e)
+        {
+            player.Stop();
+            player.Close();
+        }
+    }
+}
